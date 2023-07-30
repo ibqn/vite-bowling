87d2fcbc844e074dd3e6dcc43137547ca0229ced
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-07-30 09:50:42 +0200

    working on bowling frame

--- src/components/bowling-frame.tsx
+++ src/components/bowling-frame.tsx
@@ -1,5 +1,39 @@
-type Props = {}
+import { frameIndex } from '@/state'
+import { Frame, FrameIndex } from '@/types'
+import { cn } from '@/util/class-names'
+
+type Props = {
+  frame: Frame
+  frameNumber: FrameIndex
+}
 
 export const BowlingFrame = (props: Props) => {
-  return <div>BowlingFrame</div>
+  const { frameNumber, frame } = props
+
+  const setCurrentFrame = () => {
+    frameIndex.value = frameNumber
+  }
+
+  return (
+    <div
+      className={cn(
+        frameNumber === frameIndex.value ? 'bg-red-500' : 'bg-teal-300',
+        'w-20 cursor-pointer select-none border border-slate-500'
+      )}
+      onClick={setCurrentFrame}
+    >
+      <div className="border-b border-slate-500 p-1 text-center">
+        {frameNumber}
+      </div>
+      <div className="flex flex-row">
+        <div className="flex-1 p-1 text-center">3</div>
+        {frameNumber === 10 && <div className="flex-1 p-1 text-center">7</div>}
+        <div className="flex-1 border-b border-l border-slate-500 p-1 text-center">
+          /
+        </div>
+      </div>
+
+      <div className="p-1 text-center">{frame?.score ?? 'score'}</div>
+    </div>
+  )
 }
