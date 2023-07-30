0f477afb54b7753a87577cf5234a994d245c4098
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-07-30 09:51:29 +0200

    working on bowling game

--- src/components/bowling-game.tsx
+++ src/components/bowling-game.tsx
@@ -1,5 +1,25 @@
+import { BowlingFrame } from '@/components/bowling-frame'
+import { frames } from '@/state'
+import { FrameIndex } from '@/types'
+
 type Props = {}
 
 export const BowlingGame = (props: Props) => {
-  return <div>BowlingGame</div>
+  return (
+    <div className="flex flex-col items-center">
+      <h1>BowlingGame</h1>
+
+      <div className="flex flex-row gap-1">
+        {frames.value.map((frame, index) => {
+          return (
+            <BowlingFrame
+              key={index}
+              frameNumber={(index + 1) as FrameIndex}
+              frame={frame}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
