e5f5197532bf2820a8ffc7ab4e208316db54e696
ibqn <evgen.ibqn@gmail.com>
2023-08-05 15:11:41 +0200

    style heading

--- src/components/bowling-game.tsx
+++ src/components/bowling-game.tsx
@@ -7,7 +7,7 @@ type Props = {}
 export const BowlingGame = (props: Props) => {
   return (
     <div className="flex flex-col items-center">
-      <h1>BowlingGame</h1>
+      <h1 className="mb-2 text-2xl font-bold">Bowling Game</h1>
 
       <div className="flex flex-row gap-1">
         {frames.value.map((frame, index) => {
