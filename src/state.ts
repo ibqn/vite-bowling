411c70d8bb35acd8d5513941d7484d25b4d63c96
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-08-10 20:11:07 +0200

    throw in frame is always present

--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,8 @@
 import { signal } from '@preact/signals'
 import { type Frame, type FrameIndex } from '@/types'
 
-export const frames = signal(Array.from({ length: 10 }, () => ({}) as Frame))
+export const frames = signal(
+  Array.from({ length: 10 }, () => ({ throw: [] }) as Frame)
+)
 
 export const frameIndex = signal<FrameIndex>(1)
