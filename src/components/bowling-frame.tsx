636dd88f1d82bf8a4e9c5d1e2efd0029938ccc01
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-08-12 19:45:53 +0200

    working frame display

--- src/components/bowling-frame.tsx
+++ src/components/bowling-frame.tsx
@@ -1,6 +1,7 @@
 import { frameIndex } from '@/state'
 import { Frame, FrameIndex } from '@/types'
 import { cn } from '@/util/class-names'
+import { type ComponentChildren } from 'preact'
 
 type Props = {
   frame: Frame
@@ -12,6 +13,38 @@ export const BowlingFrame = (props: Props) => {
 
   const setCurrentFrame = () => {
     frameIndex.value = frameNumber
+  }
+
+  const firstThrow = frame.throw.at(0)
+  const secondThrow = frame.throw.at(1)
+  const thirdThrow = frame.throw.at(2)
+
+  let firstDisplay: ComponentChildren = firstThrow
+  let secondDisplay: ComponentChildren = secondThrow
+  let thirdDisplay: ComponentChildren = thirdThrow
+
+  if (firstThrow === 10) {
+    firstDisplay = undefined
+    secondDisplay = 'x'
+  } else if ((firstThrow ?? 0) + (secondThrow ?? 0) === 10) {
+    secondDisplay = '/'
+  }
+
+  if (frameNumber === 10) {
+    if (thirdThrow === 10) {
+      thirdDisplay = 'x'
+    }
+
+    if (firstThrow === 10) {
+      firstDisplay = 'x'
+      secondDisplay = secondThrow
+
+      if (secondThrow === 10) {
+        secondDisplay = 'x'
+      } else if ((secondThrow ?? 0) + (thirdThrow ?? 0) === 10) {
+        thirdDisplay = '/'
+      }
+    }
   }
 
   return (
@@ -29,11 +62,19 @@ export const BowlingFrame = (props: Props) => {
         {frameNumber}
       </div>
       <div className="flex flex-row">
-        <div className="flex-1 p-1 text-center">3</div>
-        {frameNumber === 10 && <div className="flex-1 p-1 text-center">7</div>}
-        <div className="flex-1 border-b border-l border-slate-500 p-1 text-center">
-          /
+        <div className="flex-1 p-1 text-center">
+          {firstDisplay ?? <>&nbsp;</>}
         </div>
+
+        <div className="flex-1 border-b border-l border-slate-500 p-1 text-center">
+          {secondDisplay ?? <>&nbsp;</>}
+        </div>
+
+        {frameNumber === 10 && (firstThrow ?? 0) + (secondThrow ?? 0) >= 10 && (
+          <div className="flex-1 border-b border-l border-slate-500 p-1 text-center">
+            {thirdDisplay ?? <>&nbsp;</>}
+          </div>
+        )}
       </div>
 
       <div className="p-1 text-center">{frame?.score ?? 'score'}</div>
