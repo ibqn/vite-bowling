11b80da6e809ec91c82b2b496bb3b7dfe50e6ac9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-08-16 20:28:08 +0200

    working on score update

--- src/components/throw-input.tsx
+++ src/components/throw-input.tsx
@@ -19,6 +19,88 @@ export const ThrowInput = () => {
     return Array.from({ length: remainingPins + 1 }, (_, index) => index)
   }, [state.context])
 
+  const updateScore = () => {
+    let currentScore = 0
+
+    for (const [index, frame] of frames.value.entries()) {
+      if (frame?.score) {
+        currentScore = frame.score
+        continue
+      }
+
+      if (index + 1 < 10) {
+        if (frame.throw.at(0) !== 10 && frame.throw.length < 2) {
+          return
+        }
+      } else if (
+        ((frame.throw.at(0) ?? 0) + (frame.throw.at(1) ?? 0) >= 10 &&
+          frame.throw.length !== 3) ||
+        frame.throw.length < 2
+      ) {
+        return
+      }
+
+      const frameScore = frame.throw.reduce(
+        (total: number, current): number => total + (current ?? 0),
+        0
+      )
+
+      let bonusScore = 0
+      if (index + 1 < 10) {
+        if (frame.throw.at(0) === 10) {
+          const nextFrame = frames.value.at(index + 1)
+
+          if (!nextFrame) {
+            return
+          }
+
+          if (nextFrame.throw.length === 2) {
+            bonusScore =
+              (nextFrame.throw.at(0) ?? 0) + (nextFrame.throw.at(1) ?? 0)
+          } else if (nextFrame.throw.length === 1) {
+            const secondNextFrame = frames.value.at(index + 2)
+
+            if (!secondNextFrame) {
+              return
+            }
+
+            if (secondNextFrame.throw.length > 0) {
+              bonusScore =
+                (nextFrame.throw.at(0) ?? 0) +
+                (secondNextFrame.throw.at(0) ?? 0)
+            } else {
+              return
+            }
+          } else {
+            return
+          }
+        } else if ((frame.throw.at(0) ?? 0) + (frame.throw.at(1) ?? 0) === 10) {
+          const nextFrame = frames.value.at(index + 1)
+
+          if (!nextFrame) {
+            return
+          }
+
+          if (nextFrame.throw.length > 0) {
+            bonusScore = nextFrame.throw.at(0) ?? 0
+          } else {
+            return
+          }
+        }
+      }
+
+      currentScore += frameScore + bonusScore
+
+      frames.value = frames.value.map((frame, frameIndex) => {
+        if (index === frameIndex) {
+          return { ...frame, score: currentScore }
+        }
+
+        return frame
+      })
+    }
+  }
+
   const handleInput = (pins: number) => () => {
     send({ type: 'THROW', throw: pins })
 
@@ -29,6 +111,8 @@ export const ThrowInput = () => {
 
       return frame
     })
+
+    updateScore()
   }
 
   if (state.matches('completed')) {
