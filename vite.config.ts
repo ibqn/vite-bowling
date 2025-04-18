11359c14b0893ca8e72eda6a0ab943f317dd1c8e
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-04-18 12:09:59 +0200

    refactor: replace PostCSS Tailwind plugin with Vite Tailwind plugin and remove PostCSS config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  plugins: [preact()],
+  plugins: [preact(), tailwindcss()],
 })
